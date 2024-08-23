22cd3a4955dd763e210d658a820e99d685f4b0b0
Tarang Panchal <panchaltarang18@gmail.com>
2024-08-23 18:04:02 +0530

    fix: responsive view of tables

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -44,8 +44,8 @@
                 <a href="{{ route('admin.users.create') }}" class="btn btn-default"><i class="fa fa-plus"></i> Add Data</a>
             </div>
         </div>
-        <div class="card-body">
-            <table class="ui celled table" id="data-table">
+        <div class="card-body table-responsive">
+            <table class="table table-bordered" id="data-table">
                 <thead>
                     <tr>
                         <th><input type="checkbox" class="multi-select-all"></th>
